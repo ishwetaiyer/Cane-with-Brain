f8760696a93ff22e2872b355bc4a855a04ec4c52
ishwetaiyer <35923216+ishwetaiyer@users.noreply.github.com>
2019-09-26 20:40:27 -0700

    Add files via upload

---
+++ vector_relocate.h
@@ -0,0 +1,27 @@
+#ifndef __vector_relocate_H
+#define __vector_relocate_H
+
+/*The following lines of code perform interrupt vector relocation to work with the USB bootloader. These must be
+used with every application program to run as a USB application.*/
+extern void _startup (void);
+#pragma code _RESET_INTERRUPT_VECTOR = 0x1000
+
+void _reset (void)
+{
+        _asm goto _startup _endasm
+}
+
+#pragma code
+#pragma code _HIGH_INTERRUPT_VECTOR = 0x1008
+void high_ISR (void)
+{
+}
+
+#pragma code
+#pragma code _LOW_INTERRUPT_VECTOR = 0x1018
+void low_ISR (void)
+{
+}
+#pragma code
+
+#endif
